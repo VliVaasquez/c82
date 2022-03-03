7144bfea753b749a2ec0dc2d1e8e261a869bfab9
VliVaasquez <96764787+VliVaasquez@users.noreply.github.com>
2022-03-02 23:40:00 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+(mouseEvent == "mouseDown") {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+    
